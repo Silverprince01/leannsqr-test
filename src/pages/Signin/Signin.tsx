5bc627e3b8bbc2764f5495da6129d25aa11ab1ce
Silverprince01 <87282717+Silverprince01@users.noreply.github.com>
2023-03-23 12:16:04 +0100

    Created a sign in pages

---
+++ src/pages/Signin/Signin.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+// import {Signin} from
+import { Signin, Logo } from "../../assets/index";
+import "./Signin.scss";
+export const SigninPage = () => {
+  return (
+    <div className="signin">
+      <div className="intro">
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="pablo">
+          <img src={Signin} alt="" />
+        </div>
+      </div>
+      <div className="login">
+        <h3>Welcome!</h3>
+        <p className="enter">Enter details to login.</p>
+        <form action="">
+          <input type="text" placeholder="Email" />
+          <div className="password">
+
+          <input type="password" name="" id="" placeholder="Password" />
+          <span>SHOW</span>
+          </div>
+          <p className="forgot">FORGOT PASSWORD?</p>
+          <div className="btn">
+            <Link to="/landingpage">
+            <button> LOG IN</button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
