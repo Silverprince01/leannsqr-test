1e4fd5e7c83bb5a87d42a484322ecb13dcdc6b3c
Silverprince01 <87282717+Silverprince01@users.noreply.github.com>
2023-03-23 12:21:25 +0100

    Created a landing page for the App

---
+++ src/pages/LandingPage/components/Landing.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect, useContext } from "react";
+import Loader from "../../../components/Loader";
+import { Users } from "../../../components/Users/Users";
+import { UserLists } from "../../../components/UsersList/UserLists";
+import { ValueContext } from "../../../components/context";
+import "./Landing.scss";
+export const Landing = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { name,email,organisation,phone } = useContext(ValueContext);
+  
+  const [org, setOrg] = organisation;
+  const [nam, setName] = name;
+  const [mail, setMail] = email;
+  const [phon, setPhone] = phone;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users`
+        );
+        const result = await response.json();
+        setLoading(false);
+        console.log(result);
+        setData(result);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const userData = data.filter((dat: any) => {
+    return (
+      dat.userName.toString().toLowerCase().includes(nam.toLowerCase()) &&
+      dat.orgName.toLowerCase().includes(org.toLowerCase()) &&
+      dat.email.toLowerCase().includes(mail.toLowerCase()) &&
+      dat.phoneNumber.toLowerCase().includes(phon.toLowerCase())
+    );
+  });
+  return (
+    <div className="landing">
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Users userData={userData} />
+          <UserLists userData={userData} />
+        </div>
+      )}
+    </div>
+  );
+};
