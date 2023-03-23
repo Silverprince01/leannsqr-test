d4afd2c763e42b1bfda7f1bd5858487b36e56e63
Silverprince01 <87282717+Silverprince01@users.noreply.github.com>
2023-03-23 13:09:49 +0100

    Removed unused component

--- src/pages/UserDetail/index.tsx
+++ src/pages/UserDetail/index.tsx
@@ -1,6 +1,5 @@
 import { Layout } from "../../components/Layout/Layout"
 import { UserDetailSection } from "./components/UserDetails"
-import Loader from "../../components/Loader"
 export const UserDetail = () => {
   return (
     <div>
