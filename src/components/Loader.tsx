4a045d181345b4a9f26b40e0f8f55ab3ab5f0ada
Silverprince01 <87282717+Silverprince01@users.noreply.github.com>
2023-03-23 12:19:56 +0100

    A component to show loading when waiting for the data

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="loader-container w-full h-[400px] flex items-center justify-center">
+      <span className="loader relative w-16 h-16"></span>
+    </div>
+  );
+};
+
+export default Loader;
